70250e9422ee5c09b1bd96ebac72a697d6c4cacd
cympfh <cympfh@gmail.com>
2016-04-07 21:00:24 +0900

    alias ls

--- sh/alias.sh
+++ sh/alias.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
 case $( uname -s ) in
+
   Linux )
-
-alias l='ls --color'
-alias ll='ls -lh --color'
-alias mplayer='mplayer -vo x11 -zoom -af scaletempo -speed 1.05'
-alias z='zathura'
-alias mozc-tool='/usr/lib/mozc/mozc_tool --mode=config_dialog'
-
+    alias l='ls --color'
+    alias ls='ls --color'
+    alias ll='ls -lh --color'
+    alias mplayer='mplayer -vo x11 -zoom -af scaletempo -speed 1.05'
+    alias z='zathura'
+    alias mozc-tool='/usr/lib/mozc/mozc_tool --mode=config_dialog'
     ;;
+
   * )
-
-alias l='ls -G'
-alias ll='ls -lh -G'
-
+    alias l='ls -G'
+    alias ls='ls -G'
+    alias ll='ls -lh -G'
     ;;
+
 esac
 
 alias u='cd ..'
