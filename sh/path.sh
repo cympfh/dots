9dc7efb9461176c448b0fc80809e7f045d174f59
cympfh <cympfh@gmail.com>
2016-04-09 00:05:49 +0900

    Mac PATH, local path is priority

--- sh/path.sh
+++ sh/path.sh
@@ -1,13 +1,16 @@
-export PATH=/usr/local/git/bin:$PATH
 export PATH=$PATH:/usr/local/sbin
 export PATH=$PATH:$HOME/Tools/twurl/bin
-export PATH=/usr/local/texlive/2014/bin/x86_64-darwin/:$PATH
 
-export PATH=$PATH:$HOME/bin
-export PATH=$PATH:$HOME/bin/stuff
+export PATH=$HOME/bin:$PATH
+export PATH=$HOME/bin/stuff:$PATH
 export PATH=$PATH:$HOME/bin/stuff/tw/bin
 export PATH=$PATH:$HOME/bin/stuff/language-template
 
-export PATH=$PATH:/root/.gem/ruby/2.1.0/bin
-
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
+
+case $( uname ) in
+  Darwin )
+    export PATH=/usr/local/git/bin:$PATH
+    export PATH=$PATH:/Library/TeX/texbin
+    ;;
+esac
