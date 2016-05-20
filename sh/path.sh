c31a2e91998c6bb6c7c2064c0b60eb1da9bc84d9
cympfh <cympfh@gmail.com>
2016-05-20 16:16:05 +0900

    pyenv PATH

--- sh/path.sh
+++ sh/path.sh
@@ -14,3 +14,9 @@ case $( uname ) in
     export PATH=$PATH:/Library/TeX/texbin
     ;;
 esac
+
+if [ -d ~/.pyenv ] ; then
+  export PYENV_ROOT=$HOME/.pyenv
+  export PATH=$PYENV_ROOT/bin:$PATH
+  eval "$( pyenv init - )"
+fi
