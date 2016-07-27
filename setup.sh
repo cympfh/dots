345522620679889c5431780ae9a9aab36b4aa2d7
cympfh <cympfh@gmail.com>
2016-07-27 15:44:05 +0900

    setup.sh deploys dots files on home

---
+++ setup.sh
@@ -0,0 +1,13 @@
+cat <<EOM > ~/.vimrc
+source ~/.dots/vimrc
+source ~/.dots/vim/appearance.novel.vim
+EOM
+
+cat <<EOM > ~/.screenrc
+source ~/.dots/screenrc
+source ~/.dots/screen/novel
+EOM
+
+cat <<EOM > ~/.zshrc
+source ~/.dots/zshrc
+EOM
