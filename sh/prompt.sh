fe8c002f1783364bb9e6aef98582dd0bf0a52416
cympfh <cympfh@gmail.com>
2016-09-07 11:14:06 +0900

    Remove RPROMPT

--- sh/prompt.sh
+++ sh/prompt.sh
@@ -2,12 +2,3 @@ SPROMPT="correct %R to %r? (Yes, No, Abort, Edit) "
 PROMPT="
 %F{red}%*%f %F{yellow}${HOST}:%~%f
    "
-
-chpwd() {
-    if [ -d .git ]; then
-        BRANCH=$( git branch | grep '^*' | awk '{print "[" $2 "]"}' )
-        RPROMPT="%F{blue}${BRANCH}%f"
-    else
-        RPROMPT=""
-    fi
-}
