7c2fb3cc81c778bd06689fd1e6dab0d5db610e6d
cympfh <cympfh@gmail.com>
2016-09-07 11:05:07 +0900

    PROMPT=current-branch when in git

--- sh/prompt.sh
+++ sh/prompt.sh
@@ -2,3 +2,12 @@ SPROMPT="correct %R to %r? (Yes, No, Abort, Edit) "
 PROMPT="
 %F{red}%*%f %F{yellow}${HOST}:%~%f
    "
+
+chpwd() {
+    if [ -d .git ]; then
+        BRANCH=$( git branch | grep '^*' | awk '{print "[" $2 "]"}' )
+        RPROMPT="%F{blue}${BRANCH}%f"
+    else
+        RPROMPT=""
+    fi
+}
