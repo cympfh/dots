8f26ed3d7f7cef7e1bbf622c57f8256a7344f7ac
cympfh <cympfh@gmail.com>
2016-10-14 11:34:58 +0900

    sh/screen :smile: support CentOS

--- sh/screen.sh
+++ sh/screen.sh
@@ -1,12 +1,12 @@
 case $TERM in
-  screen-256color-bce)
-    preexec() {
-      echo -ne "\ek$1\e\\"
-    }
-    precmd() {
-      #echo -ne "\ek$(basename $SHELL)\e\\"
-      echo -ne "\ek$(pwd|sed -e 's#/home/[^/]*#~#' -e 's#/Users/[^/]*#~#')\e\\"
-    }
+    screen-* )
+        preexec() {
+          echo -ne "\ek$1\e\\"
+        }
+        precmd() {
+          #echo -ne "\ek$(basename $SHELL)\e\\"
+          echo -ne "\ek$(pwd|sed -e 's#/home/[^/]*#~#' -e 's#/Users/[^/]*#~#')\e\\"
+        }
     ;;
 esac
 
