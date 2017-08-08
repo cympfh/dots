51e89000ee2fea6c741b9450fc105d38028e4065
whoami <whose@mail.ai>
2017-08-09 01:47:03 +0900

    fix

--- i3/bar.rb
+++ i3/bar.rb
@@ -26,7 +26,7 @@ Color = {
 #
 
 def get_date
-  `LANG=en date "+%Y-%m-%d (%a) %H:%M"`.chomp
+  `LANG=en date "+%Y-%m-%d(%a)%H:%M"`.chomp
 end
 
 def get_tenki
@@ -47,7 +47,7 @@ def get_tenki
   #   and my API key is cc78d27e7519b67719a1121d90e67426
   #   below url contains "example" API key
   w = JSON.parse `curl -s "http://api.openweathermap.org/data/2.5/weather?id=1850147&appid=cc78d27e7519b67719a1121d90e67426"`.chomp
-  "#{tenki_icon(w['weather'][0]['main'])}  #{k2c(w['main']['temp'])}°C #{w['main']['pressure']} hPa"
+  "#{tenki_icon(w['weather'][0]['main'])}  #{k2c(w['main']['temp'])}°C #{w['main']['pressure']}hPa"
 end
 
 def get_battery
@@ -56,7 +56,7 @@ def get_battery
   remain = result.match(/[0-9]*%/)[0].to_i
   icon = battery_icons[ [remain, 99].min / 20 ]
   is_charging = result.match(/Charging/)
-  ["#{icon} #{remain}", is_charging]
+  ["#{icon} #{remain}%", is_charging]
 end
 
 #
@@ -90,7 +90,7 @@ loop do
   columns << {full_text: battery, color: is_charging ? Color[:yellow] : Color[:orange], separator: false}
 
   date = get_date
-  columns << {full_text: date, color: Color[:pink], separator: false}
+  columns << {full_text: date, color: Color[:gray], separator: false}
 
   write columns
   sleep 1
