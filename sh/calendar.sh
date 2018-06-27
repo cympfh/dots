968198e7e0235af62ef0b3228f80249a2f506d69
cympfh <cympfh@gmail.com>
2018-06-27 16:13:27 +0900

    Improvement: calendar notify

--- sh/calendar.sh
+++ sh/calendar.sh
@@ -1,4 +1,6 @@
 CALENDAR_HOME=$HOME/Dropbox/cal
 function c {
   calendar -f $CALENDAR_HOME/index.calendar --color $@
+  NOTIFY=$HOME/Dropbox/bin/slack-calendar
+  [ -f $NOTIFY ] && $NOTIFY >/dev/null
 }
