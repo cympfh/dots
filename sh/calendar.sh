fbcae48ec626fc307fbb8b21e9bf23de362db5d0
cympfh <cympfh@gmail.com>
2018-07-04 10:30:26 +0900

    Add: c -n option

--- sh/calendar.sh
+++ sh/calendar.sh
@@ -1,6 +1,9 @@
 CALENDAR_HOME=$HOME/Dropbox/cal
 function c {
   calendar -f $CALENDAR_HOME/index.calendar --color $@
-  NOTIFY=$HOME/Dropbox/bin/slack-calendar
-  [ -f $NOTIFY ] && $NOTIFY >/dev/null
+
+  if [ "_$1" != "_-N" -a "_$1" != "_-n" ]; then
+      NOTIFY=$HOME/Dropbox/bin/slack-calendar
+      [ -f $NOTIFY ] && $NOTIFY >/dev/null
+  fi
 }
