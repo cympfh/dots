5403faece826c97e58b549ea3d5e39e5d774c712
cympfh <cympfh@gmail.com>
2018-10-29 17:30:08 +0900

    Add fuck.sh and :recycle:

--- sh/syntax-hightlight.sh
+++ sh/syntax-hightlight.sh
@@ -3,5 +3,12 @@
 # git@github.com:zsh-users/zsh-syntax-highlighting.git
 #
 
-SRC=$HOME/git/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-[[ -f $SRC ]] && source "$SRC"
+REPO_DIR=$HOME/git/zsh-syntax-highlighting
+SOURCE=$REPO_DIR/zsh-syntax-highlighting.zsh
+
+if [ -f $SOURCE ]; then
+    source $SOURCE
+else
+    echo "Not found $REPO_DIR"
+    echo "Hint: git clone git@github.com:zsh-users/zsh-syntax-highlighting.git ~/git/zsh-syntax-highlighting"
+fi
