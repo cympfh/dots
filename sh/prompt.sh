1a305363b77aba7f6b9fc9aa0c06c1b339fdee02
cympfh <cympfh@gmail.com>
2020-02-27 15:26:33 +0900

    Fix prompt.sh

--- sh/prompt.sh
+++ sh/prompt.sh
@@ -34,15 +34,14 @@ prompt-git-status() {
         return
     fi
 
-    # reponame=$(git remote show origin -n | grep Push | sed 's/^.*://g; s/.git$//g')
-    branchname=$(git branch 2>/dev/null | grep '*' | sed 's/\* //')
+    local branchname=$(git branch 2>/dev/null | grep '*' | sed 's/\* //')
 
     if [ ! -z "$(git status --short 2>/dev/null)" ]; then  # something to commit
-        statuscolor="%F{red}"
+        local statuscolor="%F{red}"
     elif git status 2>/dev/null | grep "branch is ahead" >/dev/null; then  # something to push
-        statuscolor="%F{81}"
+        local statuscolor="%F{81}"
     else
-        statuscolor="%F{246}"
+        local statuscolor="%F{246}"
     fi
 
     prompt-colon
