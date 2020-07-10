f647dbc36b20936db21a4686cdb1ce99e883922f
/c/ympfh <cympfh@gmail.com>
2020-07-11 01:43:41 +0900

    Shell PATH Manager (#3)
    * impl shell-path-manager
    
    * fix NAME
    
    * use higher API
    
    * sort PATHs

--- sh/path.sh
+++ sh/path.sh
@@ -1,67 +1,17 @@
-path-initial() {
-    [ -f /tmp/zsh-paths-lock ] && return
-    echo $PATH | sed 's/:/\n/g' |
-        while read p; do
-            echo 1 $p
-        done | sort | uniq > /tmp/zsh-paths
-}
-
-addpath() {
-    [ -f /tmp/zsh-paths-lock ] && return
-    pathdir="$1"
-    priority=${2:-1}
-    case "$1" in
-        "/*" )
-            if [ -d "$pathdir" ]; then
-                echo "$priority" "$pathdir" >> /tmp/zsh-paths
-            fi
-            ;;
-        * )
-            echo "$priority" "$pathdir" >> /tmp/zsh-paths
-            ;;
-    esac
-}
-
-path-refresh() {
-    export PATH=$(
-        cat /tmp/zsh-paths |
-            sort -u -k 2,2 |
-            sort -nr |
-            awk '{print $2}' |
-            tr '\n' ':' |
-            sed 's/:$//'
-        )
-}
-
-path-lock() {
-    touch /tmp/zsh-paths-lock
-}
-
-path-unlock() {
-    rm /tmp/zsh-paths-lock
-}
-
-path-initial
-
-# system bin
-addpath /bin 1
-addpath /usr/bin 1
-addpath /usr/local/bin 1
-
-# user bin
-addpath $HOME/bin 10
-addpath $HOME/bin/stuff 10
-addpath $HOME/.local/bin 10
-addpath $HOME/Dropbox/bin 10
-addpath $HOME/Dropbox/bin 10
-addpath $HOME/Tools/twurl/bin 10
-addpath $HOME/git/twurl/bin 10
-addpath $HOME/git/mastodon-cli/bin 10
-addpath $HOME/git/tw/bin 10
-addpath $HOME/git/mdc/ 10
-addpath $HOME/git/language-template 10
-addpath $HOME/git/imgur 10
-addpath $HOME/dw/bin 10
-addpath $HOME/Dropbox/mls 10
+shpath add system /bin
+shpath add system /usr/bin
+shpath add system-prior /usr/local/bin
+shpath add user $HOME/bin
+shpath add user $HOME/.local/bin
+shpath add user $HOME/Dropbox/bin
+shpath add user $HOME/Dropbox/mls
+shpath add user $HOME/Tools/twurl/bin
+shpath add user $HOME/dw/bin
+shpath add user $HOME/git/imgur
+shpath add user $HOME/git/language-template
+shpath add user $HOME/git/mastodon-cli/bin
+shpath add user $HOME/git/mdc/
+shpath add user $HOME/git/tw/bin
+shpath add user $HOME/git/twurl/bin
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
---
+++ sh/shell-path-manager.zsh
@@ -0,0 +1,102 @@
+shpath() {
+    case "$1" in
+        a* | add )
+            shpath-add "$2" "$3"
+            ;;
+        i* | init )
+            shpath-init
+            ;;
+        r* | refresh )
+            shpath-refresh
+            ;;
+        * )
+            shpath-help
+            ;;
+    esac
+}
+
+shpath-help() {
+    cat <<EOM
+NAME
+    shpath - Shell PATH Manager
+
+SYNOPSIS
+    shpath [subcommand] [args...]
+
+SUBCOMMAND
+
+    shpath init
+        Call this first in .zshrc.
+
+    shpath refresh
+        Call this lastly, or after shpath-add.
+        \$PATH be updated by this.
+
+    shpath add [PRIORITY] [DIRECTORY]
+
+        PRIORITY
+            You can specify from the following:
+            1. system
+            2. system-prior
+            3. user
+            4. user-prior
+            The later has larger primary.
+            NOTE: The original \$PATH before .zshrc are added as the lowest primary ("system").
+
+        DIRECTORY
+            The directory path.
+            This checks the existence, when the path is absolute (iff begins with '/').
+EOM
+}
+
+shpath-init() {
+    PATH_SYSTEM=
+    PATH_SYSTEM_PRIOR=
+    PATH_USER=
+    PATH_USER_PRIOR=
+}
+
+shpath-add() {
+    primary="$1"
+    directory="$2"
+    if [[ "$directory" =~ "^/" ]] && [ ! -d "$directory" ]; then
+        return  # no existence
+    fi
+    case "$primary" in
+        system )
+            PATH_SYSTEM="$directory:$PATH_SYSTEM"
+            ;;
+        system-prior )
+            PATH_SYSTEM_PRIOR="$directory:$PATH_SYSTEM_PRIOR"
+            ;;
+        user )
+            PATH_USER="$directory:$PATH_USER"
+            ;;
+        user-prior )
+            PATH_USER_PRIOR="$directory:$PATH_USER_PRIOR"
+            ;;
+        * )
+            echo "[Error] shpath-add: Unknown primary: $primary"
+            ;;
+    esac
+}
+
+shpath-refresh() {
+    PATH_TMP=$(
+        (
+            echo $PATH_USER_PRIOR | tr ':' '\n' | sed '/^$/d' | sed 's/^/0 /g'
+            echo $PATH_USER | tr ':' '\n' | sed '/^$/d' | sed 's/^/1 /g'
+            echo $PATH_SYSTEM_PRIOR | tr ':' '\n' | sed '/^$/d' | sed 's/^/2 /g'
+            echo $PATH_SYSTEM | tr ':' '\n' | sed '/^$/d' | sed 's/^/3 /g'
+            echo $PATH | tr ':' '\n' | sed '/^$/d' | sed 's/^/4 /g'
+        ) |
+        sort -u -k 2,2 |
+        sort -n -k 1,1 | awk '{print $2}' | tr '\n' ':'
+    )
+    PATH=$PATH_TMP
+    unset PATH_TMP
+    unset PATH_SYSTEM
+    unset PATH_SYSTEM_PRIOR
+    unset PATH_USER
+    unset PATH_USER_PRIOR
+}
