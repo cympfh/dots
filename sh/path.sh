717432a3167989794e8d4c32ad593c34c52de820
cympfh <cympfh@gmail.com>
2020-07-11 01:42:50 +0900

    sort PATHs

--- sh/path.sh
+++ sh/path.sh
@@ -2,18 +2,16 @@ shpath add system /bin
 shpath add system /usr/bin
 shpath add system-prior /usr/local/bin
 shpath add user $HOME/bin
-shpath add user $HOME/bin/stuff
 shpath add user $HOME/.local/bin
 shpath add user $HOME/Dropbox/bin
-shpath add user $HOME/Dropbox/bin
-shpath add user $HOME/Tools/twurl/bin
-shpath add user $HOME/git/twurl/bin
-shpath add user $HOME/git/mastodon-cli/bin
-shpath add user $HOME/git/tw/bin
-shpath add user $HOME/git/mdc/
-shpath add user $HOME/git/language-template
-shpath add user $HOME/git/imgur
-shpath add user $HOME/dw/bin
 shpath add user $HOME/Dropbox/mls
+shpath add user $HOME/Tools/twurl/bin
+shpath add user $HOME/dw/bin
+shpath add user $HOME/git/imgur
+shpath add user $HOME/git/language-template
+shpath add user $HOME/git/mastodon-cli/bin
+shpath add user $HOME/git/mdc/
+shpath add user $HOME/git/tw/bin
+shpath add user $HOME/git/twurl/bin
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
