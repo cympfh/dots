93d9ffd5057fec45324345968e751defed841523
cympfh <cympfh@gmail.com>
2021-11-02 21:48:31 +0900

    Bugfix; for d032558267128aad75d425e7051fccb97598e367

--- sh/screen.sh
+++ sh/screen.sh
@@ -1,48 +1,35 @@
-case $TERM in
-    screen-* )
-        preexec() {
-          echo -ne "\ek$1\e\\"
-        }
-        precmd() {
-          echo -ne "\ek$(pwd|sed -e 's#/home/[^/]*#~#' -e 's#/Users/[^/]*#~#')\e\\"
-        }
-    ;;
-esac
-
-case "$TERM" in
-    screen-* )
-        echo -ne "\ek$(pwd|sed -e 's#/home/[^/]*#~#' -e 's#/Users/[^/]*#~#')\e\\"
-        ;;
-esac
+if [ "$STY" ]; then
+    preexec() {
+      echo -ne "\ek$1\e\\"
+    }
+    precmd() {
+      echo -ne "\ek$(pwd|sed -e 's#/home/[^/]*#~#' -e 's#/Users/[^/]*#~#')\e\\"
+    }
+    echo -ne "\ek$(pwd|sed -e 's#/home/[^/]*#~#' -e 's#/Users/[^/]*#~#')\e\\"
+fi
 
 screen-x() {
-    case "$TERM" in
-        *screen* )
-            echo "You are already in screen"
-            ;;
-        * )
-            SCREENNAME=$(
-                screen -ls | grep "^	" |
-                    sed 's/^\t[0-9]*\.//g; s/\t.*//g' |
-                    peco
-            )
-            if [ "${SCREENNAME}" ]; then
-                echo -ne "\033]0;${SCREENNAME}\007"
-                screen -x "${SCREENNAME}"
-            fi
-            ;;
-    esac
+    if [ "$STY" ]; then
+        echo "You are already in screen"
+    else
+        SCREENNAME=$(
+            screen -ls | grep "^	" |
+                sed 's/^\t[0-9]*\.//g; s/\t.*//g' |
+                peco
+        )
+        if [ "${SCREENNAME}" ]; then
+            echo -ne "\033]0;${SCREENNAME}\007"
+            screen -x "${SCREENNAME}"
+        fi
+    fi
 }
 
 screen-s() {
-    case "$TERM" in
-        *screen* )
-            echo "You are already in screen"
-            ;;
-        * )
-            SCREENNAME=${1:-$(basename $(pwd))}
-            echo -ne "\033]0;${SCREENNAME}\007"
-            screen -S "${SCREENNAME}"
-            ;;
-    esac
+    if [ "$STY" ]; then
+        echo "You are already in screen"
+    else
+        SCREENNAME=${1:-$(basename $(pwd))}
+        echo -ne "\033]0;${SCREENNAME}\007"
+        screen -S "${SCREENNAME}"
+    fi
 }
