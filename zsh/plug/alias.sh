03d8c1b6f4570c7cc31b29da2cc3811a59681475
cympfh <cympfh@gmail.com>
2022-05-16 14:37:06 +0900

    Rename alias x=exa, :fire: alias l for ls

--- zsh/plug/alias.sh
+++ zsh/plug/alias.sh
@@ -1,26 +1,10 @@
-case $( uname -s ) in
-
-  Linux )
-    alias l='ls --color'
-    alias ls='ls --color'
-    alias ll='ls -lh --color'
-    alias mplayer='mplayer -vo x11 -zoom -af scaletempo -speed 1.05 -correct-pts'
-    alias mozc-tool='/usr/lib/mozc/mozc_tool --mode=config_dialog'
-    ;;
-
-  * )
-    alias l='ls -G'
-    alias ls='ls -G'
-    alias ll='ls -lh -G'
-    ;;
-
-esac
-
 alias g='git'
 alias vi='vim'
 alias nv='nvim'
 alias j='LC_ALL=C $EDITOR -c JournalNew'
-alias e='exa'
+alias x='exa --icons'
+alias mplayer='mplayer -vo x11 -zoom -af scaletempo -speed 1.05 -correct-pts'
+alias mozc-tool='/usr/lib/mozc/mozc_tool --mode=config_dialog'
 
 mkcd() {
     mkdir "$1" && cd "$1"
