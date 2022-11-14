52ed735c358f6e42eb1d44846669a65db886c024
cympfh <cympfh@gmail.com>
2022-11-14 14:35:29 +0900

    :new: zsh/plug/editor.sh

---
+++ zsh/plug/editor.sh
@@ -0,0 +1,10 @@
+if ( command -v nvim >/dev/null ); then
+  export EDITOR=nvim
+elif ( command -v vim >/dev/null ); then
+  export EDITOR=vim
+elif ( command -v vi >/dev/null ); then
+  export EDITOR=vi
+else
+  export EDITOR=
+fi
+alias e=$EDITOR
