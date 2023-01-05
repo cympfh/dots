c6d94a4042922aeeb2befca876ae8c69a3a1245b
cympfh <cympfh@gmail.com>
2023-01-05 16:35:17 +0900

    alias xla

--- zsh/plug/alias.sh
+++ zsh/plug/alias.sh
@@ -4,6 +4,7 @@ alias nv='nvim'
 alias j='LC_ALL=C $EDITOR -c JournalNew'
 alias x='exa --icons'
 alias xl='exa -l --icons'
+alias xla='exa -la --icons'
 alias mplayer='mplayer -vo x11 -zoom -af scaletempo -speed 1.05 -correct-pts'
 alias mozc-tool='/usr/lib/mozc/mozc_tool --mode=config_dialog'
 
