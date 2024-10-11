468770ee70806defb84873799c6f6462ce5644ea
whoami <none@example.com>
2024-10-11 18:07:45 +0900

    update zsh path

--- zsh/plug/path.sh
+++ zsh/plug/path.sh
@@ -11,22 +11,16 @@ shpath add user $HOME/bin
 shpath add user $HOME/.local/bin
 shpath add user $HOME/Dropbox/bin
 shpath add user $HOME/Dropbox/mls
-shpath add user $HOME/Tools/twurl/bin
 shpath add user $HOME/dw/bin
 shpath add user $HOME/dw/git/khattori/amadeus/bin
 shpath add user $HOME/git/imgur
 shpath add user $HOME/git/language-template
 shpath add user $HOME/git/mastodon-cli/bin
-shpath add user $HOME/git/mdc/
-shpath add user $HOME/git/tw/bin
-shpath add user $HOME/git/twurl/bin
 shpath add user $HOME/git/mline/bin
 shpath add user $HOME/test/bin
-shpath add user $HOME/git/morph/bin
 shpath add user $HOME/git/hf/bin
 shpath add user $HOME/git/ac/bin
-shpath add user $HOME/git/grow.md/bin
-shpath add user $HOME/.yarn/bin
+shpath add user $HOME/git/grow.md.fasthtml/bin
 shpath add user $HOME/.deno/bin
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
